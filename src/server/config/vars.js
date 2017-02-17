87d6212d0b26c444c9ffe32916c0eb507269abb7
sandman <sandman8654@outlook.com>
2017-02-17 02:31:31 -0800

    configuration for local mongodb

--- src/server/config/vars.js
+++ src/server/config/vars.js
@@ -1,4 +1,5 @@
 module.exports = {
     COOKIE_SECRET: '33e4e1ab40aba2c64aa5a1ee80efe33774de6c0a0f0a7339d759c8518e0eef8791af68b6f3e6ccbe068e87606eeaab4e4eb538fbe5e60a18dc01bdd6db8ebd92',
-    MONGO_URI: 'mongodb://shopshot:shopshot@ds131099.mlab.com:31099/shopshot',
+  //  MONGO_URI: 'mongodb://shopshot:shopshot@ds131099.mlab.com:31099/shopshot',
+	MONGO_URI: 'mongodb://shopshot:shopshot@ds131099.mlab.com:27017/shopshot'
 };
